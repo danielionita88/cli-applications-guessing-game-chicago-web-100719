fd68ad4dfc9634e81a7c962104be3268002df7e6
Daniel I <danielionita88@yahoo.com>
2019-10-03 20:58:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,17 +1,17 @@
 
 def run_guessing_game
- generate_number = rand(6) + 1
+generate_number = rand(6) + 1
  
- puts 'Welcome to "Guess The Number"! Please choose a number from 1 to 6!'
+puts 'Welcome to "Guess The Number"! Please choose a number from 1 to 6!'
  
- user_input = gets.chomp 
+user_input = gets.chomp 
  
- if user_input.to_i == generate_number
-   puts "You guessed the correct number!"
- elsif user_input == "exit"
-   puts "Goodbye!"
- else puts "Sorry! The computer guessed #{generate_number}!"
- end
+if user_input.to_i == generate_number
+  puts "You guessed the correct number!"
+elsif user_input == "exit"
+  puts "Goodbye!"
+else puts "Sorry! The computer guessed #{generate_number}!"
+end
    
  
 end
